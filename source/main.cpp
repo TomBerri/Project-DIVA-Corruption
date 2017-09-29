3c4e28415f688101a8f51b82d72099f34c7d4830
TomBerri <thomasb2685@gmail.com>
2017-09-29 23:22:59 +0100

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -5,14 +5,14 @@
 
 int main (int argc, char *argv[]) {
 	
-	CreateDirectory("PDRX Files", NULL);
+	CreateDirectory("PDRX Files", NULL); // Creates the directory for all randomised files created
 
-	if (argc == 2 && (strcmp(argv[1], "fr") == 0 || strcmp(argv[1], "FR") == 0)) {
+	if (argc == 2 && (strcmp(argv[1], "fr") == 0 || strcmp(argv[1], "FR") == 0)) { // firstread randomiser
 		FR_rand();
-	} else if (argc == 2 && (strcmp(argv[1], "dsc") == 0 || strcmp(argv[1], "DSC") == 0)) {
+	} else if (argc == 2 && (strcmp(argv[1], "dsc") == 0 || strcmp(argv[1], "DSC") == 0)) { // DSCs randomiser
 		DSC_rand();
 	} else {
-		fprintf(stderr, "Invalid Argument: Use fr/FR for firstread.bin or dsc/DSC for any/all DSC");
+		fprintf(stderr, "Invalid Argument: Use fr/FR for firstread.bin or dsc/DSC for any/all DSC"); // Bad input argumentZ
 	}
 
 	return 0;
